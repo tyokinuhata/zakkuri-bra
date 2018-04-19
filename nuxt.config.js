c73b0840bbdc00991749cfec3a255a3fe214840a
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-19 17:51:07 +0900

    Build

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Oppai boin boin' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/zakkuri-bra/favicon.ico' }
     ]
   },
   /*
@@ -34,6 +34,9 @@ module.exports = {
         })
       }
     }
+  },
+  router: {
+    base: '/zakkuri-bra/'
   },
   modules: [
     'bootstrap-vue/nuxt',
