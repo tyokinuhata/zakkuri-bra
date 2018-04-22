4e9b54bdf69618eca198d42505a6c34e9ce95bd5
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-22 21:51:39 +0900

    Add links

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="container mt-4">
-    <h1 class="text-center">おっぱいのサイズを、<br>カップ数でざっくり。</h1>
-    <div class="text-center">
-      <div class="row mt-2">
-        <label for="top" class="col-md-2">トップ&nbsp;</label>
-        <input type="number" id="top" class="form-control col-md-8" v-model="top" @change="calcDiff(top, under); calcCup(top, under)" autofocus>
-        <span class="col-md-2">cm</span>
-      </div>
-      <div class="row mt-2">
-        <label for="under" class="col-md-2">アンダー&nbsp;</label>
-        <input type="number" id="under" class="form-control col-md-8" v-model="under" @change="calcDiff(top, under); calcCup(top, under)">
-        <span class="col-md-2">cm</span>
-      </div>
-      <div class="row">
-        <span class="col-md-12 mt-2 h2" v-if="top < under || under < 0">おっぱいがおかしいです</span>
-        <span class="col-md-12 mt-2 h2" v-else-if="diff < 0">おっぱいが空洞化しています</span>
-        <span class="col-md-12 mt-2 h2" v-else-if="diff === 0">おっぱいがありません</span>
-        <span class="col-md-12 mt-2 h2" v-else-if="diff >= 75">おっぱいが大きすぎます</span>
-        <span class="col-md-12 mt-2 h2" v-else>あなたのカップ数は{{ cup }}です</span>
+  <div>
+    <div class="text-right">
+      <a href="https://github.com/" target="_blank" class="mr-2">GitHub</a>
+      <a href="https://twitter.com/h_tyokinuhata" target="_blank" class="mr-2">Twitter</a>
+      <a href="https://tyokinuhata.github.io/portfolio/" target="_blank" class="mr-2">Other</a>
+    </div>
+    <div class="container mt-4">
+      <h1 class="text-center">おっぱいのサイズを、<br>カップ数でざっくり。</h1>
+      <div class="text-center">
+        <div class="row mt-2">
+          <label for="top" class="col-md-2">トップ&nbsp;</label>
+          <input type="number" id="top" class="form-control col-md-8" v-model="top" @change="calcDiff(top, under); calcCup(top, under)" autofocus>
+          <span class="col-md-2">cm</span>
+        </div>
+        <div class="row mt-2">
+          <label for="under" class="col-md-2">アンダー&nbsp;</label>
+          <input type="number" id="under" class="form-control col-md-8" v-model="under" @change="calcDiff(top, under); calcCup(top, under)">
+          <span class="col-md-2">cm</span>
+        </div>
+        <div class="row">
+          <span class="col-md-12 mt-2 h2" v-if="top < under || under < 0">おっぱいがおかしいです</span>
+          <span class="col-md-12 mt-2 h2" v-else-if="diff < 0">おっぱいが空洞化しています</span>
+          <span class="col-md-12 mt-2 h2" v-else-if="diff === 0">おっぱいがありません</span>
+          <span class="col-md-12 mt-2 h2" v-else-if="diff >= 75">おっぱいが大きすぎます</span>
+          <span class="col-md-12 mt-2 h2" v-else>あなたのカップ数は{{ cup }}です</span>
+        </div>
       </div>
     </div>
   </div>
